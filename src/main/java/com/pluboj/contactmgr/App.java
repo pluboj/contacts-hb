7de3be04e89be3e707058ff2233d14b045856b6b
pluboj <pluboj@yahoo.com>
2017-12-13 15:26:14 -0800

    Add SessionFactory

--- src/main/java/com/pluboj/contactmgr/App.java
+++ src/main/java/com/pluboj/contactmgr/App.java
@@ -1,6 +1,19 @@
 package com.pluboj.contactmgr;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+
 public class App {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String[] args) {
 
     }
